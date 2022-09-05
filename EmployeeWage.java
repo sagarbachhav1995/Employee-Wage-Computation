5b30658040e568341d6a757c388cdc0deb8bc0f2
sagarbachhav1995 <sagarbachhav8@gmail.com>
2022-09-05 01:45:26 -0700

    Added: Employee Full Time salary

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -7,14 +7,22 @@ public class EmployeeWage {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Employee Wage Computation Program");
+		int wagePerHour=20;
+		int fullDayHours=8;
+		int salary=0;
+		
 		Random random = new Random();
 		int randomNum = random.nextInt(2);
 
 		if (randomNum == 1)
+		{
 			System.out.println("Employee is Present");
+			salary = wagePerHour * fullDayHours;
+		}
 		else
 			System.out.println("Employee is Absent");
-
+		
+		System.out.println("Employee salary is : "+salary);
 	}
 
 }
