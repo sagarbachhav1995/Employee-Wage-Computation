c28da9886331b44e807b77710fc5d8854075d1ed
sagarbachhav1995 <sagarbachhav8@gmail.com>
2022-09-05 01:27:43 -0700

    Added: Employee Present Or Absent

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,10 +1,20 @@
 package com.blz.employee.wage;
 
+import java.util.Random;
+
 public class EmployeeWage {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Employee Wage Computation Program");
+		Random random = new Random();
+		int randomNum = random.nextInt(2);
+
+		if (randomNum == 1)
+			System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is Absent");
+
 	}
 
 }
